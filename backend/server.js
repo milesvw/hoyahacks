cb42bc3f628bae467e3c0bef88bef61d5c0b56fa
milesvw <milesvenderwilson@gmail.com>
2025-01-24 22:03:07 -0500

    Generated base server

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('CokeVote backend is running!');
+});
+
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
