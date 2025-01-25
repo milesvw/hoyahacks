71f5b497eb3da2f783539b608e154619439ed78a
milesvw <milesvenderwilson@gmail.com>
2025-01-25 14:42:11 -0500

    Remove highcharts watermark

--- frontend/scripts.js
+++ frontend/scripts.js
@@ -1,24 +1,29 @@
 //chart
 document.addEventListener('DOMContentLoaded', function () {
-    const chart = Highcharts.chart('container', {
-        chart: {
-            type: 'bar'
-        },
-        title: {
-            text: 'Results'
-        },
-        xAxis: {
-            categories: ['Coke', 'Diet Coke', 'Coke Zero']
-        },
-        yAxis: {
-            title: {
-                text: 'Votes'
-            }
-        },
-        series: [{
-            name: 'Jane',
-            selected:true,
-            data: [1, 20, 4]
-        }]
-    });
+  const chart = Highcharts.chart('container', {
+    chart: {
+      type: 'bar',
+    },
+    title: {
+      text: 'Results',
+    },
+    xAxis: {
+      categories: ['Coke', 'Diet Coke', 'Coke Zero'],
+    },
+    yAxis: {
+      title: {
+        text: 'Votes',
+      },
+    },
+    credits: {
+      enabled: false,
+    },
+    series: [
+      {
+        name: 'Jane',
+        selected: true,
+        data: [1, 20, 4],
+      },
+    ],
+  });
 });
